37f8994141f880a746c23e706478781d5b4e214e
Monika_Bhasin <monika.bhasin@myparkplus.com>
2021-09-18 22:24:05 +0530

    change in cusitom button

--- app/src/main/java/com/hk/customview/HKButton.kt
+++ app/src/main/java/com/hk/customview/HKButton.kt
@@ -2,7 +2,9 @@ package com.hk.customview
 
 import android.annotation.SuppressLint
 import android.content.Context
+import android.content.res.ColorStateList
 import android.content.res.TypedArray
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
@@ -18,41 +20,80 @@ class HKButton(context: Context, attrs: AttributeSet) : LinearLayout(context, at
         inflater.inflate(R.layout.hk_button, this, true)
         val attributeArray: TypedArray = context.theme.obtainStyledAttributes(
             attrs, R.styleable.HKButton, 0, 0)
-        val hkButtonStyle: HKButtonStyle
+
         attributeArray.let {
             setStyle(
                 hkButtonStyle {
+                    text = it.getString(R.styleable.HKButton_text)
                     backgroundColor = it.getColor(R.styleable.HKButton_backgroundColor, -1)
                     backgroundAlpha = it.getColor(R.styleable.HKButton_backgroundAlpha, -1)
                     isBorder = it.getBoolean(R.styleable.HKButton_isBorder, false)
                     borderColor = it.getColor(R.styleable.HKButton_borderColor, -1)
                     borderStroke = it.getColor(R.styleable.HKButton_borderStroke, -1)
-                    borderAlpha = it.getColor(R.styleable.HKButton_borderAlpha, -1)
                     isIcon = it.getBoolean(R.styleable.HKButton_isIcon, false)
+                    rippleColor = it.getColor(R.styleable.HKButton_rippleColor, -1)
+                    iconPosition = IconPosition.values()[it.getInt(R.styleable.HKButton_iconPosition, 0)]
+                    iconDrawable = it.getDrawable(R.styleable.HKButton_iconDrawable)
                 }
             )
         }
         attributeArray.recycle()
     }
 
+    enum class IconPosition {
+        RIGHT, LEFT
+    }
+
     private fun setStyle(hkButtonStyle: HKButtonStyle) {
         (getChildAt(0) as MaterialButton).run {
             hkButtonStyle.backgroundColor?.let {
                 this.setBackgroundColor(it)
-                this.text = "Hare Krsna"
+            }
+            hkButtonStyle.text?.let {
+                this.text = it
+            }
+            hkButtonStyle.isBorder?.let { isBorder ->
+                if(isBorder) {
+                    hkButtonStyle.borderColor?.let {
+                        if(it!=-1) {
+                            this.strokeColor = ColorStateList.valueOf(it)
+                        }
+                    }
+                    hkButtonStyle.borderStroke?.let {
+                        if(it!=-1) {
+                            this.strokeWidth = it
+                        }
+                    }
+                }
+            }
+            hkButtonStyle.rippleColor?.let {
+                if(it != -1) {
+                    this.rippleColor = ColorStateList.valueOf(it)
+                }
+            }
+            hkButtonStyle.isIcon?.let { isIcon ->
+                if(isIcon) {
+                    hkButtonStyle.iconDrawable?.let {
+                        this.icon = it
+                        this.iconGravity = MaterialButton.ICON_GRAVITY_START
+                    }
+                }
             }
         }
     }
 }
 
 class HKButtonStyle {
+    var text: String? = null
     var backgroundColor: Int? = null
     var backgroundAlpha: Int? = null
     var isBorder: Boolean? = null
     var borderColor: Int? = null
     var borderStroke: Int? = null
-    var borderAlpha: Int? = null
     var isIcon: Boolean? = null
+    var rippleColor: Int? = null
+    var iconDrawable: Drawable? = null
+    var iconPosition : HKButton.IconPosition? = null
 }
 
 fun hkButtonStyle(lambda : HKButtonStyle.() -> (Unit)) : HKButtonStyle {
